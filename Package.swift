2010fe3a4dc86227ca0594aa215cf0bf4fcfab96
Sean Soper <sean.soper@gmail.com>
2016-01-07 13:52:34 -0500

    use ssoper inquiline for now

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
   name: "Curassow",
   dependencies: [
     .Package(url: "https://github.com/nestproject/Nest.git", majorVersion: 0, minor: 2),
-    .Package(url: "https://github.com/nestproject/Inquiline.git", majorVersion: 0, minor: 2),
+    .Package(url: "https://github.com/ssoper/Inquiline.git", majorVersion: 0, minor: 2),
     .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0, minor: 4),
   ],
   testDependencies: [
